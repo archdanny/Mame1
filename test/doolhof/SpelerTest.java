7e04eae12566797fa4fab457bd1afe8b0d838a0b
mem0ow <turk0ow_mem0ow@live.nl>
2015-06-08 16:12:34 +0200

    new doolhof

--- test/doolhof/SpelerTest.java
+++ test/doolhof/SpelerTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package doolhof;
 
 import static doolhof.Direction.Up;
@@ -18,22 +17,22 @@ import org.junit.*;
  * @author Danny
  */
 public class SpelerTest {
-    
+
     public SpelerTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -50,59 +49,57 @@ public class SpelerTest {
 //        // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
 //    }
-
     /**
-     * Test of move method With a direction 
+     * Test of move method With a direction
      */
-    @Test
+    @Test // JUnit test 1
     public void testMoveMethod() {
 
         Direction direction = Right;
         Speler instance = new Speler();
-        
+
         Veld veld = new Veld();
-        
+
         Veld Noord = new Veld();
         Veld Zuid = new Veld();
         Veld West = new Veld();
         Veld Oost = new Veld();
 
-        veld.fillBuurMap(Noord, Zuid, West ,Oost);
- 
+        veld.fillBuurMap(Noord, Zuid, West, Oost);
+
         instance.setVeld(veld);
         instance.move(direction);
         assertEquals(instance.getVeld(), Oost);
     }
-    
-    
-       @Test
+
+    @Test  // JUnit test 2
     public void testOldVeld() {
 
         Direction direction = Up;
         Speler instance = new Speler();
-        
+
         Veld veld = new Veld();
-        
+
         Veld Noord = new Veld();
         Veld Zuid = new Veld();
         Veld West = new Veld();
         Veld Oost = new Veld();
 
-        veld.fillBuurMap(Noord, Zuid, West ,Oost);
- 
+        veld.fillBuurMap(Noord, Zuid, West, Oost);
+
         instance.setVeld(veld);
         instance.move(direction);
         assertEquals(veld.getItem(), null);
     }
-    
-      @Test
+
+    @Test // JUnit test 3
     public void testMoveWall() {
 
         Direction direction = Up;
         Speler instance = new Speler();
-        
+
         Veld veld = new Veld();
-        
+
         Veld Noord = new Veld();
         Muur muur = new Muur(false);
         Noord.setItem(muur);
@@ -110,24 +107,22 @@ public class SpelerTest {
         Veld West = new Veld();
         Veld Oost = new Veld();
 
-        veld.fillBuurMap(Noord, Zuid, West ,Oost);
- 
+        veld.fillBuurMap(Noord, Zuid, West, Oost);
+
         instance.setVeld(veld);
         instance.move(direction);
         assertEquals(instance.getVeld(), veld);
-        
+
     }
-    
-    
-     
-      @Test
+
+    @Test // JUnit test 4
     public void testBazookaPickUp() {
 
         Direction direction = Up;
         Speler instance = new Speler();
-        
+
         Veld veld = new Veld();
-        
+
         Veld Noord = new Veld();
         Bazooka bazooka = new Bazooka();
         Noord.setItem(bazooka);
@@ -135,13 +130,45 @@ public class SpelerTest {
         Veld West = new Veld();
         Veld Oost = new Veld();
 
-        veld.fillBuurMap(Noord, Zuid, West ,Oost);
- 
+        veld.fillBuurMap(Noord, Zuid, West, Oost);
+
         instance.setVeld(veld);
         instance.move(direction);
         assertEquals(instance.getBazooka(), bazooka);
-        
+
     }
 
+    @Test  // JUnit test 5
+    public void testBazookaSchieten() {
+        Bazooka bazooka = new Bazooka();
+        for (int k = 0; k < 3; k++) {
+            Raket raket = new Raket();
+            bazooka.setRakets().add(raket);
+        }
+        Speler instance = new Speler();
 
+        Direction direction = Up;
+        
+
+        Veld veld = new Veld();
+
+        Veld Noord = new Veld();
+        Veld Noord2 = new Veld();  
+        Noord.setItem(bazooka);        
+        Noord2.setItem(new Muur(false));
+        Veld Zuid = new Veld();
+        Veld West = new Veld();
+        Veld Oost = new Veld();
+
+        veld.fillBuurMap(Noord, Zuid, West, Oost);
+        Noord.fillBuurMap(Noord2, Zuid, West, Oost);
+        instance.setVeld(veld);
+        instance.move(direction);
+        instance.schieten();
+        instance.schieten();
+        instance.schieten();
+        
+        assertEquals(bazooka.getKogels(), 3);
+
+    }
 }
